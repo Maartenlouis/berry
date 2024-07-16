abe51351afa281419d6798ee140dbe79e05d7d39
maartenvo <maarten.vonoesen@gmail.com>
2024-07-16 19:22:11 +0200

    changed plausible domain3

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,27 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      // NextJS <Image> component needs to whitelist domains for src={}
+      "lh3.googleusercontent.com",
+      "pbs.twimg.com",
+      "images.unsplash.com",
+      "logos-world.net",
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/plausible/js/script.js",
+        destination: "https://plausible.io/js/script.js",
+      },
+      {
+        source: "/plausible/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
+};
 
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = nextConfig;
