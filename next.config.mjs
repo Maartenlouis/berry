17bb88bc077f5ae1f09850a17693f6374927d49c
maartenvo <maarten.vonoesen@gmail.com>
2024-07-16 19:16:26 +0200

    changed plausible domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const { withPlausibleProxy } = require("next-plausible");
+
+module.exports = withPlausibleProxy()({
+  // ...your next js config, if any
+});
+
 const nextConfig = {};
 
 export default nextConfig;
