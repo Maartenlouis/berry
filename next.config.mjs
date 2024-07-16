4906743fa571b2d3162c698eae19958b309497f7
maartenvo <maarten.vonoesen@gmail.com>
2024-07-16 19:18:30 +0200

    changed plausible domain2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const { withPlausibleProxy } = require("next-plausible");
-
-module.exports = withPlausibleProxy()({
-  // ...your next js config, if any
-});
 
 const nextConfig = {};
 
